0d58c127e276647ac75f518ef29a36ba40eaba85
Hec98 <hector982015@gmail.com>
2022-01-22 22:54:26 -0600

    Add primitives

---
+++ assets/js/primitives.js
@@ -0,0 +1,26 @@
+let name = 'Peter Parker';
+console.log(name);
+
+name = 'Ben Parker';
+console.log(name);
+
+name = "May"
+name = `May`
+console.log(typeof name);
+
+let esMarvel = true;
+console.log(typeof esMarvel)
+
+age = 23;
+console.log(typeof age)
+
+let superPower;
+console.log(typeof superPower)
+
+let amNull = null;
+console.log(typeof amNull)
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log(typeof symbol1)
+console.log(symbol1 === symbol2)
