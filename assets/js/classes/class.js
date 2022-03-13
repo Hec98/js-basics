d5213e708f8bd765e4360542c022100ef4554706
Hec98 <hector982015@gmail.com>
2022-03-12 21:37:37 -0600

    sets and gets

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -2,11 +2,32 @@ class Person {
     name = '';
     code = ''; 
     sentence = '';
+    food = '';
 
     constructor(name='Nameless', code='Without code', sentence='No sentence') {
         this.name = name;
         this.code = code;
         this.sentence = sentence;
+    }
+
+    /**
+     * @param {string} food
+     */
+    set setFavoriteFood(food) {
+        this.food = food.toUpperCase();
+    }
+
+    get getFavoriteFood() {
+        return `${this.name} favorite food is ${this.food}`;
+    }
+
+    whoIAm() {
+        console.log(`I am ${this.name} and my identity is ${this.code}`);
+    }
+
+    mySentence() {
+        this.whoIAm
+        console.log(`${this.code} says ${this.sentence}`)
     }
 }
 
@@ -14,3 +35,13 @@ const spiderman = new Person('Peter Parker', 'Spiderman', `I'm your friendly nei
 const ironman = new Person('Tony Stark', 'Ironman', `I am Iron Man`);
 console.log(spiderman);
 console.log(ironman);
+
+spiderman.mySentence();
+ironman.whoIAm();
+ironman.mySentence();
+
+spiderman.setFavoriteFood = `Aunt May's Cherry Pie`;
+
+console.log(spiderman);
+console.log(spiderman.getFavoriteFood);
+
